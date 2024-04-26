fbc58388da0cdea074f78bfedf5566ff1d57d77c
Anas Sarkiz <anessarkez4@gmail.com>
2024-04-26 19:16:31 +0200

    Created slide components

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,8 +1,31 @@
-const Slide = ({ element }: { element: any }) => {
+"use client";
+const toCategories: () => void = () => {
+  console.log("It Works");
+};
+const Slide = ({
+  name,
+  backgroundImage,
+  description,
+}: {
+  name: string;
+  backgroundImage: string;
+  description: string;
+}) => {
   return (
     <section id="do not touch">
-      <div className="w-[80vw] h-[80vh] border-2 border-black">
-        <div>{element} slide</div>
+      <div className="w-[100%] h-[100vh]">
+        <div className={`${backgroundImage} h-full bg-cover relative`}>
+          <div className="absolute top-[35%] left-[60%] ">
+            <h1 className="text-white  ">{name}</h1>
+            <p className="text-white ">{description}</p>
+            <button
+              onClick={toCategories}
+              className="bg-transparent hover:bg-orange-400 text- font-semibold hover:text-white py-2 px-10 border border-orange-700 hover:border-transparent rounded border-"
+            >
+              Click Me
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
