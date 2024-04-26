3728ce7c3e9ed30142fc8e4600db83f7b3e6b4d9
AbseLY <abdaslam13.2.2001@gmail.com>
2024-04-26 19:30:16 +0200

    started with home component

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,9 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+function useIsClient() {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return isClient;
+}
 const Home = () => {
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+  const reportWin = () => {
+    setWidth(window.innerWidth);
+  };
+  if (useIsClient()) window.onresize = reportWin;
+
   return (
     <section id="do not touch">
-      <div className="w-[80vw] h-[80vh] border-2 border-black">
-        <div>home</div>
-      </div>
+      {width > 560 ? (
+        <div className="h-[100vh] overflow-hidden flex items-center justify-center relative -z-10 ">
+          <iframe
+            src="https://player.vimeo.com/video/939643388?autoplay=1&color=ff0179&title=0&byline=0&portrait=0&loop=1&controls=0&muted=1&quality=1080p"
+            allow="autoplay"
+            className="w-[300%] h-[129%] absolute"
+          ></iframe>
+          <h1 className="z-10 text-white  text-5xl font-bold">AlsahmAdv</h1>
+        </div>
+      ) : (
+        <div className="w-[100%] h-[100vh] overflow-hidden flex items-center justify-center relative -z-10">
+          <iframe
+            src="https://player.vimeo.com/video/939621491?autoplay=1&color=ff0179&title=0&byline=0&portrait=0&loop=1&controls=0&muted=1&quality=1080p"
+            allow="autoplay"
+            className="w-[140%] h-[105%] absolute"
+          ></iframe>
+          <h1 className="z-10 text-white  text-5xl font-bold">AlsahmAdv</h1>
+        </div>
+      )}
     </section>
   );
 };
