a353ba126639e860e43ec544a9c3540ed674c339
Anas Sarkiz <anessarkez4@gmail.com>
2024-06-08 19:33:12 +0200

    Modified navBar and CatNav functionalty

--- components/CatNav.tsx
+++ components/CatNav.tsx
@@ -1,35 +1,78 @@
 "use client";
-
-import CatTabs from "./CatTab";
+import React, { useRef, useState } from "react";
 import navIcon from "../assets/navIcon.svg";
-import { useState } from "react";
+import CatTabs from "./CatTab";
 
-const tabs = [
-  { name: "Potato1", icon: navIcon },
-  { name: "Potato2", icon: navIcon },
-  { name: "Potato3", icon: navIcon },
-  { name: "Potato4", icon: navIcon },
-];
-
-const CatNav = () => {
+export default function CatNav() {
+  const tabs = [
+    { name: "Potato1", icon: navIcon },
+    { name: "Potato2", icon: navIcon },
+    { name: "Potato3", icon: navIcon },
+    { name: "Potato4", icon: navIcon },
+  ];
   const [selectedTab, setSelectedTab] = useState(tabs[0].name);
+  const itemRef = useRef<HTMLDivElement>(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollingLeft, setScrollingLeft] = useState(0);
+
+  if (itemRef.current?.scrollLeft === 0) itemRef.current?.scrollTo(3000, 0);
 
   return (
-    <section className="w-[100%] flex   pt-16 pb-4 md:px-[10%]">
-      <div className="w-full flex items-center bg-gradient-to-tl to to-mySeaBlue from-myDarkBlue justify-evenly gap-6   border-2 border-myDarkBlue  md:rounded-full">
-        {tabs.map((tab, index) => {
-          return (
-            <CatTabs
-              key={index}
-              tab={tab}
-              selectedTab={selectedTab}
-              setSelectedTab={setSelectedTab}
-            />
-          );
-        })}
-      </div>
-    </section>
-  );
-};
+    <>
+      <div className="flex  w-screen    justify-center ">
+        <div
+          className="category  items-centers w-[50%]  "
+          ref={itemRef}
+          onMouseDown={(e) => {
+            setIsMouseDown(true);
 
-export default CatNav;
+            setStartX(
+              e.pageX - (itemRef.current?.getBoundingClientRect().left || 0)
+            );
+            setScrollingLeft(itemRef.current?.scrollLeft || 0);
+            console.log(
+              e.pageX - (itemRef.current?.getBoundingClientRect().left || 0),
+              "scrollingLeft"
+            );
+          }}
+          onMouseLeave={() => {
+            setIsMouseDown(false);
+            console.log("leave");
+          }}
+          onMouseUp={() => {
+            setIsMouseDown(false);
+            console.log("up");
+          }}
+          onMouseMove={(e) => {
+            if (!isMouseDown) return;
+
+            itemRef.current?.scrollTo(3000, 0);
+
+            e.preventDefault();
+            const x =
+              e.pageX - (itemRef.current?.getBoundingClientRect().left || 0);
+            const walk = x - startX; //scroll-faster
+            itemRef.current?.scrollLeft &&
+              (itemRef.current.scrollLeft = scrollingLeft - walk);
+            console.log(walk, "walk");
+            console.log(itemRef.current?.scrollLeft, "x");
+          }}
+        >
+          {Array.from({ length: 30 }).map((_, index) => (
+            <div key={index} className="category-slide flex justify-between">
+              {tabs.map((tab, tabIndex) => (
+                <CatTabs
+                  key={tabIndex}
+                  tab={tab}
+                  selectedTab={selectedTab}
+                  setSelectedTab={setSelectedTab}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
