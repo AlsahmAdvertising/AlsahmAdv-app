08de267d5abd383e4bd8ffa591eea1a75c530fcc
Shibo <shibosoftwaredev@gmail.com>
2025-02-17 18:39:32 +0200

    fixed index error

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -42,10 +42,10 @@ const Categories = () => {
       <CatNav />
       <div className="w-full relative  flex flex-col items-center overflow-hidden rounded-3xl h-[85%]">
         <div className="overflow-hidden flex relative w-[85%] flex-col bg-secondary shadow-primary_inside drop-shadow-custom_black_center h-[100%]  items-center  my-6 md:gap-10  ">
-          <span className="absolute overflow-visible block animate-slider1 top-0 left-[-100%] w-full h-[2px] bg-white"></span>
-          <span className="absolute block animate-slider2 right-0 top-[-100%] w-[2px] h-full bg-white"></span>
-          <span className="absolute block animate-slider3 bottom-0 right-[-100%] w-full h-[2px] bg-white"></span>
-          <span className="absolute block animate-slider4 left-0 bottom-[-100%] h-full w-[2px] bg-white"></span>
+          <span className="absolute overflow-visible block animate-slider1 z-50 top-0 left-[-100%] w-full h-[2px] bg-white"></span>
+          <span className="absolute block animate-slider2 right-0 z-50 top-[-100%] w-[2px] h-full bg-white"></span>
+          <span className="absolute block animate-slider3 bottom-0 z-50 right-[-100%] w-full h-[2px] bg-white"></span>
+          <span className="absolute block animate-slider4 left-0 z-50 bottom-[-100%] h-full w-[2px] bg-white"></span>
           <div className="min-w-[350px] justify-around w-full mt-8 pb-32 overflow-y-scroll px-[2%] flex flex-wrap">
             {!post ? (
               <div className="flex justify-center items-center text-white rounded-xl p-[2%] fade-in drop-shadow-custom w-full aspect-video sm:w-1/2 xl:w-1/3">
